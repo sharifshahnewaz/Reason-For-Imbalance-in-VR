4773d89eedad5d776701a53093f166ee1eecbbf4
Sharif Mohammad Shahnewaz Ferdous <sharif.shahnewaz@gmail.com>
2017-10-14 17:55:43 -0500

    Added instructions in this module

---
+++ Instructions and Data Measurement/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioClip readyConf;
+    public AudioClip three;
+    public AudioClip two;
+    public AudioClip one;
+    public AudioClip lookCommand;
+    public AudioClip front;
+    public AudioClip left;
+    public AudioClip right;
+    public AudioClip top;
+    public AudioClip bottom;
+
+    Dictionary<string, AudioClip> audios;
+
+    private AudioSource audioSrc;
+
+    // Use this for initialization
+    void Awake()
+    {
+        audioSrc = GetComponent<AudioSource>();
+
+        audios = new Dictionary<string, AudioClip>();
+        audios.Add("readyConf", readyConf);
+        audios.Add("three", three);
+        audios.Add("two", two);
+        audios.Add("one", one);
+        audios.Add("lookCommand", lookCommand);
+        audios.Add("front", front);
+        audios.Add("left", left);
+        audios.Add("right", right);
+        audios.Add("top", top);
+        audios.Add("bottom", bottom);
+    }
+
+    public void PlayAudio(string audioName) {
+        audioSrc.PlayOneShot(audios[audioName]);
+    }
+}
---
+++ Instructions and Data Measurement/Assets/Scripts/RecordCOP.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+public class RecordCOP : MonoBehaviour
+{
+
+    UIVA_Client_WiiFit theClient;
+    string ipUIVAServer = "127.0.0.1";
+
+    private double gravX = 0.0, gravY = 0.0, weight = 0.0, prevGravX = 0.0, prevGravY = 0.0, pathX = 0.0, pathY = 0.0, path = 0.0;
+    private bool isFirstRow = true;
+    private double tl = 0.0, tr = 0.0, bl = 0.0, br = 0.0;
+    private string fitbutt = "";
+    private double elapsedTime = 0.0f;
+    private bool isWriting = false;
+    private string displayMessage = null;
+    private StringBuilder sb;
+
+    public int sampleRate = 20;
+    public String studyCondition;
+
+    AudioPlayer audioPlayer;
+
+    void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WindowsWebPlayer ||
+            Application.platform == RuntimePlatform.OSXWebPlayer)
+        {
+            if (Security.PrefetchSocketPolicy(ipUIVAServer, 843, 500))
+            {
+                Debug.Log("Got socket policy");
+            }
+            else
+            {
+                Debug.Log("Cannot get socket policy");
+            }
+        }
+
+        audioPlayer = GetComponent<AudioPlayer>();
+    }
+
+    void Start()
+    {
+        try
+        {
+            theClient = new UIVA_Client_WiiFit(ipUIVAServer);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+        displayMessage = "Press 'R' to start recording data";
+        sb = new StringBuilder();
+        sb.Append("System Time,Elapsed Time,Gravity X,Gravity Y, PathX, PathY, Path, Weight\n");
+        StartCoroutine(WriteInFile());
+        StartCoroutine(PlayInstructions());
+
+
+    }
+
+    IEnumerator WriteInFile()
+    {
+        //yield return new WaitForSeconds (0.001f);
+        while (true)
+        {
+            try
+            {
+                theClient.GetWiiFitRawData(out tl, out tr, out bl, out br, out fitbutt);
+                theClient.GetWiiFitGravityData(out weight, out gravX, out gravY, out fitbutt);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.ToString());
+            }
+            if (!isFirstRow)
+            {
+                pathX = Math.Abs(gravX - prevGravX);
+                pathY = Math.Abs(gravY - prevGravY);
+                path = Math.Sqrt(pathX * pathX + pathY * pathY);
+            }
+            if (isWriting)
+            {
+                sb.Append(String.Format("{0},{1},{2},{3},{4},{5},{6},{7}\n", System.DateTime.Now.ToString("hh.mm.ss.ffffff"), elapsedTime, gravX, gravY, pathX, pathY, path, weight));
+                prevGravX = gravX;
+                prevGravY = gravY;
+                isFirstRow = false;
+                elapsedTime += (1.0f / sampleRate);
+            }
+            yield return new WaitForSeconds((1.0f / sampleRate));
+        }
+
+    }
+
+    string[] instructions = {
+        // start with normal navigation
+        "left", "right", "top", "bottom", "front",
+        "left", "right", "top", "bottom", "front",
+        // Do a left right (horizontal) movement
+        "left", "right", "left", "right", "left", "right",
+        "left", "right", "left", "right", "front",
+        // Do a top bottom (vertical) movement
+        "top", "bottom", "top", "bottom", "top", "bottom",
+        "top", "bottom", "top", "bottom", "front"
+    };
+
+    public float delay = 2;
+
+    IEnumerator PlayInstructions()
+    {
+        audioPlayer.PlayAudio("readyConf");
+        yield return new WaitForSeconds(2);
+        audioPlayer.PlayAudio("three");
+        yield return new WaitForSeconds(1);
+        audioPlayer.PlayAudio("two");
+        yield return new WaitForSeconds(1);
+        audioPlayer.PlayAudio("one");
+        yield return new WaitForSeconds(1);
+        audioPlayer.PlayAudio("lookCommand");
+        yield return new WaitForSeconds(1.5f);
+
+        for (int i = 0; i < instructions.Length; i++)
+        {
+            audioPlayer.PlayAudio(instructions[i]);
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (isWriting)
+            {
+                isWriting = false;
+                displayMessage = "Press 'R' to start recording data";
+            }
+            else
+            {
+                isWriting = true;
+                displayMessage = "Data is recording... Press 'R' to stop";
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = 30;
+
+        GUI.color = Color.green;
+        GUI.Label(new Rect(200, 100, Screen.width, Screen.height), displayMessage);
+
+        GUI.color = Color.white;
+        GUI.Label(new Rect(200, 200, Screen.width, Screen.height), "WiiFit Data:");
+        GUI.Label(new Rect(200, 240, Screen.width, Screen.height), "Weight:\t\t" + weight.ToString());
+        GUI.Label(new Rect(200, 280, Screen.width, Screen.height), "Grav X:\t\t" + gravX.ToString());
+        GUI.Label(new Rect(200, 320, Screen.width, Screen.height), "Grav Y:\t\t" + gravY.ToString());
+
+        GUI.color = Color.red;
+        GUI.Label(new Rect(200, 400, Screen.width, Screen.height), "WiiFit Raw Data:");
+        GUI.Label(new Rect(200, 440, Screen.width, Screen.height), "Top Left:\t\t" + tl.ToString());
+        GUI.Label(new Rect(200, 480, Screen.width, Screen.height), "Bottom Left:\t" + bl.ToString());
+        GUI.Label(new Rect(200, 520, Screen.width, Screen.height), "Top Right :\t" + tr.ToString());
+        GUI.Label(new Rect(200, 560, Screen.width, Screen.height), "Bottom Right :\t" + br.ToString());
+
+    }
+
+    void OnApplicationQuit()
+    {
+        System.IO.File.AppendAllText("Data/" + studyCondition + "-balance-" + System.DateTime.Now.Ticks.ToString() + ".csv", sb.ToString());
+    }
+}
+
+
+
+
